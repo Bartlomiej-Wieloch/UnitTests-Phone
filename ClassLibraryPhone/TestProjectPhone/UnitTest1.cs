082d93d42114b2fe6ecebbfdf47af201f7e4ffa0
Bartlomiej-Wieloch <bartlomiej.wieloch@microsoft.wsei.edu.pl>
2025-03-11 23:22:22 +0100

    Achive 100% test coverage in classlibraryphone.dll

--- ClassLibraryPhone/TestProjectPhone/UnitTest1.cs
+++ ClassLibraryPhone/TestProjectPhone/UnitTest1.cs
@@ -32,13 +32,129 @@ namespace TestProjectPhone
 
             // Act
             var telefon = new Phone(wlasciciel, numer);
+
             // Assert
 
-            //Assert.ThrowsException<ArgumentException>(() => new Phone(wlasciciel, numer));
+            //Assert.ThrowsException<ArgumentException>(() => new Phone(wlasciciel, numer)); <-albo to, albo 24 linijka
         }
-    }
-    class Pomocniczna
-    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Konstruktor_BrakNumeruTelefonu_ArgumentException()
+        {
+            // Arange
+            var wlasciciel = "Bartek";
+            var numer = "";
 
+            // Act
+            var telefon = new Phone(wlasciciel, numer);
+
+            // Assert
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Konstruktor_BrakImienia_ArgumentException()
+        {
+            // Arange
+            var wlasciciel = "";
+            var numer = "123456789";
+
+            // Act
+            var telefon = new Phone(wlasciciel, numer);
+
+            // Assert
+
+        }
+        [TestMethod]
+        public void Konstruktor_WywolanieNumeruTelefonuGetterem_Powodzenie()
+        {
+            // Arange
+            var wlasciciel = "Bartek";
+            var numer = "123456789";
+            var telefon = new Phone(wlasciciel, numer);
+
+            // Act
+
+            // Assert
+            Assert.AreEqual(numer, telefon.PhoneNumber);
+        }
+        [TestMethod]
+        public void AddContact_NowyKontakt_Powodzenie()
+        {
+            // Arange
+            var wlasciciel = "Bartek";
+            var numer = "123456789";
+            var telefon = new Phone(wlasciciel, numer);
+
+            // Act
+            telefon.AddContact(wlasciciel, numer);
+
+            // Assert
+            Assert.IsTrue(telefon.AddContact("Adam", numer));
+        }
+        [TestMethod]
+        public void AddContact_KontaktJuzIstnieje_NiePowodzenie()
+        {
+            // Arange
+            var wlasciciel = "Bartek";
+            var numer = "123456789";
+            var telefon = new Phone(wlasciciel, numer);
+
+            // Act
+            telefon.AddContact(wlasciciel, numer);
+
+            // Assert
+            Assert.IsFalse(telefon.AddContact(wlasciciel, numer));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void AddContact_KsiazkaKontakowPelna_Exception()
+        {
+            // Arange
+            var wlasciciel = "Bartek";
+            var numer = "123456789";
+            var telefon = new Phone(wlasciciel, numer);
+
+            for (int i = 0; i < telefon.PhoneBookCapacity; i++)
+            {
+                telefon.AddContact(wlasciciel + i, numer);
+            }
+
+            // Act
+            telefon.AddContact("Adam", numer);
+
+            //telefon.AddContact(wlasciciel, numer);
+            // Assert
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Call_KontaktNieIstnieje_NiePowodzenie()
+        {
+            // Arange
+            var wlasciciel = "Bartek";
+            var numer = "123456789";
+            var telefon = new Phone(wlasciciel, numer);
+
+            // Act
+            telefon.Call("Adam");
+
+            // Assert
+        }
+        [TestMethod]
+        public void Call_KontaktIstnieje_Powodzenie()
+        {
+            // Arange
+            var wlasciciel = "Bartek";
+            var numer = "123456789";
+            var telefon = new Phone(wlasciciel, numer);
+
+            string nowyKontakt = "Adam";
+            telefon.AddContact(nowyKontakt, numer);
+
+            // Act
+
+            // Assert
+            Assert.AreEqual($"Calling {numer} ({nowyKontakt}) ...", telefon.Call(nowyKontakt));
+        }
     }
 }
